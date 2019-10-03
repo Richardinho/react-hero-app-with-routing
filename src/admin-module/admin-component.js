c7d1b4b24f780a6ba84161d4c9b2456272323312
Richard Hunter <richard@richardhunter.co.uk>
2019-10-03 13:11:18 +0100

    refactoring

--- src/admin-module/admin-component.js
+++ src/admin-module/admin-component.js
@@ -11,32 +11,37 @@ export default class Admin extends Component {
       return (
         <div>
           <h3>admin</h3>
+
           <nav>
             <Link to="/admin/dashboard">dashboard</Link>
             <Link to="/admin/manage-crisis">manage crises</Link>
             <Link to="/admin/manage-heroes">manage heroes</Link>
           </nav>
 
-          <Route  path="/admin/manage-crisis" render={() => {
-            return (<div className="route">
+          <Route  path="/admin/manage-crisis" render={() => (
+            <div className="route">
               <h2>manage crisis</h2> 
-              </div>) 
-          }}/>
+            </div>
+          )}/>
 
-          <Route path="/admin/dashboard" render={() => {
-            return (<div>
+          <Route path="/admin/dashboard" render={() => (
+            <div>
               <h2>dashboard</h2> 
-            </div>) 
-          }}/>
-          <Route path="/admin/manage-heroes" render={() => {
-            return (<div>
+            </div>
+          )}/>
+          <Route path="/admin/manage-heroes" render={() => (
+            <div>
               <h2>manage heroes</h2> 
-            </div>) 
-          }}/>
+            </div> 
+          )}/>
         </div>
       )    
     } else {
-     return (<Redirect to="/login?returnto=/admin"/>);
+      /*
+       *  This causes an error to appear in the console.
+       */
+
+      return (<Redirect to="/login?returnto=/admin"/>);
     }
   }
 }
